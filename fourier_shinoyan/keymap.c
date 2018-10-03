954d514a63b81c5d2ef9cd723b39b7e1859eae97
shinoyan <taste.of.emo@gmail.com>
2018-10-03 21:05:53 +0900

    レイヤーの切り替えとBASEレイヤーのキー配置を変更

--- fourier_shinoyan/keymap.c
+++ fourier_shinoyan/keymap.c
@@ -17,9 +17,10 @@ enum custom_keycodes {
 
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
-#define KC_FN1 MO(_FN1)
-#define KC_FN2 MO(_FN2)
-#define KC_FN3 MO(_FN3)
+#define KC_BASE TO(_BASE)
+#define KC_FN1 TO(_FN1)
+#define KC_FN2 TO(_FN2)
+#define KC_FN3 TO(_FN3)
 #define KC_SPFN1 LT(_FN1, KC_SPACE)
 #define KC_BSFN2 LT(_FN2, KC_BSPC)
 #define EISU KC_LANG2
@@ -27,30 +28,30 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,  KC_BSPC,
-    KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT,          KC_ENT,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_MINUS,  KC_BSPC,
+    KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,          KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,             KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_FN1, KC_LALT, KC_LGUI, EISU,  KC_SPC, KC_SPC, KANA, KC_LGUI, KC_FN2,  KC_FN3
+    KC_BASE, KC_LALT, KC_LGUI, EISU,  KC_SPC, KC_SPC, KANA, KC_FN3, KC_FN2,  KC_FN1
   ),
 
   [_FN1] = LAYOUT(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
-    RESET,   RGB_HUI, RGB_SAI, RGB_VAI, KC_VOLU, KC_LBRC, KC_RBRC, KC_4,    KC_5,    KC_6,    KC_SCLN,          _______,
-    RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, KC_VOLD, KC_LCBR, KC_RCBR,          KC_1,    KC_2,    KC_3,    KC_UP,   _______,
-    RGB_TOG, _______, _______, _______, _______,          KC_DEL,                    KC_0,    KC_LEFT, KC_DOWN, KC_RGHT
+    KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_AT, KC_BSPC,
+    KC_LCTL,   RGB_HUI, RGB_SAI, RGB_VAI, KC_VOLU, KC_LBRC, KC_RBRC, KC_4,    KC_5,    KC_6,    KC_QUOT,        KC_ENT,
+    KC_LSFT, RGB_HUD, RGB_SAD, RGB_VAD, KC_VOLD, KC_LCBR, KC_RCBR,          KC_1,    KC_2,    KC_3,    KC_UP,   KC_RSFT,
+    KC_BASE, KC_LALT, KC_LGUI, EISU,  KC_SPC, KC_SPC, KANA, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [_FN2] = LAYOUT(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS,
     _______, _______, _______, KC_INS,  KC_PGUP, KC_HOME, _______, _______, _______, _______, KC_COLN,          _______,
     _______, _______, _______, KC_DEL,  KC_PGDN, KC_END,  _______,          _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, KC_DEL,           _______,                   _______, _______, _______, _______
+    KC_BASE, _______, _______, _______, KC_DEL,           _______,                   _______, _______, _______, _______
   ),
 
   [_FN3] = LAYOUT(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS,
     _______, _______, _______, KC_INS,  KC_PGUP, KC_HOME, _______, _______, _______, _______, KC_COLN,          _______,
     _______, _______, _______, KC_DEL,  KC_PGDN, KC_END,  _______,          _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, KC_DEL,           _______,                   _______, _______, _______, _______
+    KC_BASE, _______, _______, _______, KC_DEL,           _______,                   _______, _______, _______, _______
   )
 };
