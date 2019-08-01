032d379aaa7432b395bdd397b7f0d66ff007b636
TakuyaShinoyama <taste.of.emo@gmail.com>
2019-08-01 23:10:42 +0900

    イコールを追加

--- fourier_shinoyan/keymap.c
+++ fourier_shinoyan/keymap.c
@@ -35,9 +35,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_FN1] = LAYOUT(
-    KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_AT, KC_BSPC,
-    KC_LCTL,   RGB_HUI, RGB_SAI, RGB_VAI, KC_VOLU, KC_LBRC, KC_RBRC, KC_4,    KC_5,    KC_6,    KC_QUOT,        KC_ENT,
-    KC_LSFT, RGB_HUD, RGB_SAD, RGB_VAD, KC_VOLD, KC_LCBR, KC_RCBR,          KC_1,    KC_2,    KC_3,    KC_UP,   KC_RSFT,
+    KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, KC_BSPC,
+    KC_LCTL,   RGB_HUI, RGB_SAI, RGB_VAI, KC_VOLU,  KC_3,  KC_4,    KC_5,    KC_6,    KC_LBRC, KC_RBRC,        KC_ENT,
+    KC_LSFT, RGB_HUD, RGB_SAD, RGB_VAD, KC_VOLD, KC_LCBR, KC_RCBR,          KC_1,    KC_2,    KC_QUOT,    KC_UP,   KC_RSFT,
     KC_BASE, KC_LALT, KC_LGUI, EISU,  KC_SPC, KC_SPC, KANA, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
